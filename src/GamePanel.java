8ba03a5f26e240edb5048df48ed1261b365443f0
PriBhula <pridotbhula@gmail.com>
2019-03-17 13:32:52 +1300

    initial commit and initial window

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,26 @@
+import javax.swing.JPanel;
 
-public class GamePanel {
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class GamePanel extends JPanel {
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 480;
+
+	private static final long serialVersionUID = 1L;
+	
+	public GamePanel() {
+		super();
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setFocusable(true);
+		requestFocus();
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawString("hello world", 300, 270);
+	}
 
 }
